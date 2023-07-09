9f2214a829f671292ac3b2cb75a628e7840a7aaf
anhdung02 <padungg8@gmil.com>
2023-07-09 22:44:22 +0700

    UN-02: Màn hình kinh tê - xa hoi

--- activities/SocioEconomicController.php
+++ activities/SocioEconomicController.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use Database\DataBase;
+
 class SocioEconomicController
 {
     public function show(){
+
+        $db = new DataBase();
+        $setting = $db->select('SELECT * FROM websetting')->fetch();
+
+        $data_ = $db->select('SELECT posts.*, author.fullname AS author_name , categories.name as cat_name, categories.code_name as code_name
+                     FROM posts 
+                     INNER JOIN author ON posts.author_id = author.id
+					 INNER JOIN categories ON posts.cat_id = categories.id
+					 WHERE  categories.type = 1 ');
 
         // Xu ly logic o day
 
