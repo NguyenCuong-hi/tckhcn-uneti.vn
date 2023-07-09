cea25835644c5cb833a224f44fb14122db27a69f
nguyenhungtks7 <nguyenhungtks1@gmail.com>
2023-07-09 19:25:27 +0700

    Sua Trang chu va thong tin tra doi

--- template/app/thongtin_traodoi.php
+++ template/app/thongtin_traodoi.php
@@ -4,7 +4,225 @@ require_once(BASE_PATH . '/template/app/layouts/header.php');
 ?>
 <!--Xủ lý hiển thị ở đây-->
 
+<div style="display: flex;  margin-top: 30px; margin-right: 32px">
+        <div style="display: flex; padding-right: 32px; width: 20%; flex-direction: column; box-sizing: border-box; padding: 8px; color: white; font-size: 12px; font-weight: 700; color: white  ">
+            <div style="width: 100%; margin: 8px 0">
+                <Button style=" color: white;font-weight: 700; border-radius: 8px; background-color: #348FDE; width: 100%; border: none ; padding: 18px 18px">
+                    Trang chủ
+                </Button>
+            </div>
+            <div style="width: 100%; margin: 8px 0">
+                <Button onclick="window.location.href='<?= url('khoahoccongnghe')?>'" style="color: white; font-weight: 700;border-radius: 8px;background-color: #348FDE; width: 100%; border: none ; padding: 18px 18px">
+                    Khoa học - công nghệ
+                </Button>
+            </div>
+            <div style="width: 100%; margin: 8px 0">
+                <Button style="color: white;font-weight: 700;border-radius: 8px;background-color: #348FDE; width: 100%; border: none ; padding: 18px 18px">
+                    Kinh tế- xã hội
+                </Button>
+            </div>
+            <div style="width: 100%; margin: 8px 0 ">
+                <Button style="color: white;font-weight: 700;border-radius: 8px;background-color: #348FDE; width: 100%; border: none ; padding: 18px 18px">
+                    Diễn đàn khoa học
+                </Button>
+            </div>
+            <div style="width: 100%; margin: 8px 0 ">
+                <Button style="color: white;font-weight: 700;border-radius: 8px;background-color: #348FDE; width: 100%; border: none ; padding: 18px 18px">
+                    Thông tin trao đổi
+                </Button>
+            </div>
+            <div style="width: 100%; margin: 8px 0">
+                <Button style="color: white;font-weight: 700;border-radius: 8px;background-color: #348FDE; width: 100%; border: none ; padding: 18px 18px">
+                    Liên hệ
+                </Button>
+            </div>
+        </div>
 
+<!-- _________________________________________________________________________________ -->
+<div style="width: 60%; border: 1px solid #ddd; margin: 0 64px; padding: 4px; box-sizing: border-box">
+    <h3 style="text-transform: uppercase; color: #3734ea">Thể lệ bài gửi</h3>
+    <div style="padding-left: 20px;">
+    <p><span style="font-weight: 700;">
+        Tạp chí Khoa học & Công nghệ
+    </span> 
+    Trường Đại học 
+Kinh tế - Kỹ thuật Công nghiệp là diễn đàn trao đổi 
+khoa học và công nghệ; công bố và giới thiệu các 
+công trình nghiên cứu khoa học, ứng dụng công 
+nghệ mới, các thành tựu khoa học, kỹ thuật mới liên 
+quan tới các lĩnh vực mà Nhà trường đào tạo góp 
+phần nâng cao chất lượng đào tạo nguồn nhân lực 
+và chuyển giao công nghệ.
+
+    </p>
+    <p  style="font-weight: 700; color: #3734ea">I. Thể lệ cho tòa soạn</p>
+    <p>
+    1. Tạp chí chỉ nhận đăng những bài báo khoa học 
+chưa từng được công bố trên các báo, tạp chí 
+khoa học trong nước và quốc tế;
+<p>
+    2. Bài viết bằng tiếng Việt hoặc tiếng Anh, viết cẩn 
+thận, đúng văn phạm, hình vẽ đặt đúng chỗ, vẽ rõ 
+nét (Tòa soạn không làm lại chế bản cho hình
+vẽ);
+</p>
+<p>
+    3. Bài viết từ 5 đến 10 trang A4 (kể cả bảng biểu và 
+tài liệu tham khảo);
+</p>
+<p>4. Bài viết gửi về tòa soạn gồm: 2 bản in và file mềm 
+(theo file mẫu của Tòa soạn) qua hộp thư điện tử;
+
+</p>
+<p>
+    5. Bài viết gửi đăng không đảm bảo các qui định về
+thể lệ thì Ban biên tập có quyển từ chối và phản 
+hồi lại tác giả qua thư điện tử.
+Tòa soạn không gửi lại bài nếu không được
+đăng
+</p  style="font-weight: 700; color: #3734ea">I. Qui định khuôn khổ bài báo:
+
+    </p>
+    <p>
+    1. Báo cáo toàn văn font Times New Roman cỡ chữ
+12
+    </p>
+    <p>
+    2. Trên khổ giấy A4; phần chứa text có kích thước 
+16 x 24 cm (theo định dạng file gửi tác giả).
+    </p>
+    <p>
+    3. Phần nội dung của bài báo được chia thành 2 cột, 
+khoảng cách giữa 2 cột: 0,6 cm;
+    </p>
+    <p>
+    4. Phần hình vẽ, bảng biểu phải nằm trong khuôn 
+khổ của bài báo (chú thích hình vẽ theo thứ tự và 
+để dưới hình vẽ, chú thích bảng biểu theo thứ tự
+và đặt trên của bảng biểu), căn giữa, các bảng biểu 
+và hình vẽ quá lớn trình bày theo trang ngang
+    </p>
+    <p  style="font-weight: 700; color: #3734ea">
+    II. Bố cục và hình thức trình bày bài báo:
+    </p>
+    <p>
+    1. <span  style="font-weight: 700;">Tên bài báo:</span>  Tên bài báo cần ngắn gọn nhưng 
+thể hiện nội dung chính của bài báo, đề cập thẳng 
+đến vấn đề bài báo muốn giải quyết; được viết 
+bằng Tiếng Việt và Tiếng Anh (font chữ Times 
+New Roman cỡ chữ 16, chữ in hoa đậm, căn lề
+giữa, không chia cột).
+    </p>
+    <p>
+    2. <span  style="font-weight: 700;">Tên tác giả</span>  (viết bằng chữ in đậm) và địa chỉ của 
+tác giả (viết bằng chữ in thường) đặt ngay sau tên 
+tác giả, font Times New Roman, cỡ chữ 12, cân
+giữa;
+    </p>
+    <p>
+    3. <span  style="font-weight: 700;">Tóm tắt (Abstract): </span> được trình bày dưới dạng 
+một đoạn văn ngắn gọn (không quá 200 từ), thể
+hiện tầm quan trọng, mục đích nghiên cứu, 
+phương pháp nghiên cứu, những kết quả và kết 
+luận chủ yếu của bài báo; được viết bằng Tiếng 
+Việt và Tiếng Anh (font Times New Roman, kiểu 
+chữ thường, cỡ chữ 10, không chia cột);
+    </p>
+    <p>
+    4. <span  style="font-weight: 700;">Từ khóa (Keywords): </span>bằng Tiếng Việt và Tiếng 
+Anh, không quá 5 từ, font Times New Roman, 
+kiểu chữ thường cỡ chữ 10;
+    </p>
+    <p>
+    5. <span  style="font-weight: 700;">Nội dung: </span> Bài báo viết bằng tiếng Việt hoặc bằng 
+tiếng Anh và cần có kết quả mới (về lý thuyết, 
+phương pháp, phương tiện, mô hình, công cụ…), 
+có giá trị khoa học và thực tiễn, (font Times New
+Roman, cỡ chữ 12; Line Spacing: At least, At: 14 
+pt; Spacing: Before 6 pt, After 6 pt). Nội dung bao
+gồm:
+    </p>
+    <p>
+    • Phần mở đầu (đặt vấn đề, giới thiệu): tóm tắt tình 
+hình nghiên cứu, tính thời sự, cấp thiết, xác định 
+mục tiêu, phương pháp nghiên cứu.
+    </p>
+    <p>
+    • Nội dung: giải quyết vấn đề nghiên cứu bằng 
+những lập luận chặt chẽ, có cơ sở khoa học. Các 
+số liệu, kết quả thu được phải phản biện rõ ràng, 
+chính xác, chỉ rõ nguồn trích dẫn
+    </p>
+    <p>
+    + Các hình, ảnh, bảng, biểu cần được đánh số thứ tự, 
+trình bày rõ ràng
+    </p>
+    <p>
+    + Hình, ảnh phải được quét (scan) và đưa vào đúng 
+vị trí minh họa, độ phân giải của ảnh tối thiểu là 
+600 dpi.
+
+    </p>
+    <p>
+    • Kết luận: khẳng định lại những kết quả nghiên 
+cứu rút ra từ công trình này và nêu các đề xuất
+    </p>
+    <p>
+    6. <span  style="font-weight: 700;">Tài liệu tham khảo: </span> Tài liệu tiếng Việt ghi trước, 
+sau đó là tài liệu bằng tiếng nước khác. Khi trích 
+dẫn cần đầy đủ các thông tin sau:
+    </p>
+    <p>+ Sách: Họ tên tác giả (hoặc chủ biên), tên sách, 
+trang, nhà xuất bản, nơi, lần và năm xuất bản</p>
+<p>+ Tạp chí: Họ tên tác giả, tên bài báo, tên tạp chí, tập, 
+số, trang, năm xuất bản (năm để trong ngoặc);</p>
+<p>+ Hội nghị, hội thảo: Họ tên tác giả, tên báo cáo, 
+đơn vị tổ chức, địa điểm, năm, trang;
+</p>
+<p>
++ Luận án, luận văn: Họ tên tác giả, tên luận án 
+(luận văn), cơ quan chủ quản, năm bảo vệ;
+</p>
+<p>
++ Tài liệu lấy từ internet cần ghi rõ họ và tên tác 
+giả, thời gian công bố, tên tài liệu, đường dẫn
+</p>
+<p>
+Chú ý: Tài liệu trích dẫn phải đặt đúng vị trí ngay sau 
+các câu viết trong bài.
+</p>
+<p>
+    7. <span style="font-weight: 700;">Các thông tin liên quan đến tác giả: </span>học 
+hàm, học vị, cơ quan công tác, điện thoại, email 
+của tác giả phải ghi rõ ràng ở cuối bài.
+    </p>
+    <i style="font-weight: 700; ">Nội dung chi tiết và thông tin liên hệ xin gửi về:
+</i>
+<p  style="font-weight: 700; color: #3734ea; text-transform: uppercase;">
+TÒA SOẠN TẠP CHÍ KHOA HỌC & CÔNG 
+NGHỆ
+</p>
+<p>
+Phòng 402 – Nhà HA11, Trường Đại học Kinh tế -
+Kỹ thuật Công nghiệp, Số 218 Lĩnh Nam, Hoàng 
+Mai, Hà Nội
+</p>
+<p>
+E-mail: <a style="text-decoration: ;" href="tapchikhcn@uneti.edu.vn"><u>tapchikhcn@uneti.edu.vn</u></a>
+</p>
+<p>
+Website : <a href=" https://tckhcn-uneti.vn"><u> https://tckhcn-uneti.vn</u></a>
+</p>
+</div>
+</div>
+
+        <div style="width: 20%; padding-left: 32px">
+            <img style="width: 100%" src="../../../public/banner-image/bo-cong-thuong.png"/>
+            <img style="width: 100%" src="../../../public/banner-image/DHKTKTCN.png"/>
+            <img style="width: 100%" src="../../public/banner-image/khcn.png"/>
+
+        </div>
+    </div>
 
 <?php
 require_once(BASE_PATH . '/template/app/layouts/footer.php');
