d58aa3e7ff2e9194bd29f63574d62618eef9615b
nguyenhungtks7 <nguyenhungtks1@gmail.com>
2023-07-12 01:36:14 +0700

    Phân trang

--- activities/ScienceForumController.php
+++ activities/ScienceForumController.php
@@ -13,12 +13,30 @@ class ScienceForumController
         // Category type = 3
         $get_image_sidebar = $db->select('SELECT * FROM banners WHERE id IN (10, 11, 12)');
 
+        $item_per_page = !empty($_GET['per_page']) ? $_GET['per_page'] : 4;
+        $current_page =!empty($_GET['page']) ? $_GET['page'] : 1;
+        $off_set = ( $current_page -1)* $item_per_page;
+
         $data = $db->select('SELECT posts.*, author.fullname AS author_name , categories.name as cat_name, categories.code_name as code_name
                      FROM posts 
                      INNER JOIN author ON posts.author_id = author.id
-					 INNER JOIN categories ON posts.cat_id = categories.id WHERE type = 3;
+					 INNER JOIN categories ON posts.cat_id = categories.id LIMIT '.$item_per_page.' OFFSET '.$off_set.'
 					 ');
 
+                     $result = $db->select('SELECT COUNT(*) AS total_count
+                     FROM posts 
+                     INNER JOIN author ON posts.author_id = author.id 
+                     INNER JOIN categories ON posts.cat_id = categories.id;');
+
+
+    $row = $result->fetch();
+
+
+        $total_count = $row['total_count'];
+
+
+               $totalRecords = $row['total_count'];
+         $totalPages = ceil($totalRecords/$item_per_page);
         require_once (BASE_PATH. '/template/app/diendan_khoahoc.php');
     }
 }
