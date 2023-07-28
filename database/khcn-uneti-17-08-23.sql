de1d3100318d3798c86567f0a4e57701ab3fdf54
CuongNV <nvc.nvcuong@gmail.com>
2023-07-28 20:55:34 +0700

    UNETI-NI: add library

--- database/khcn-uneti-17-08-23.sql
+++ database/khcn-uneti-17-08-23.sql
@@ -1,27 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 17, 2023 at 05:17 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `news-project`
---
-CREATE DATABASE IF NOT EXISTS `khcn-uneti` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `khcn-uneti`;
 
 -- --------------------------------------------------------
 
